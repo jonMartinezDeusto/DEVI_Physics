b611edf3f5f113de5029f853a4be89554f76d535
Jonimarti <124839368+jonMartinezDeusto@users.noreply.github.com>
2023-03-10 08:42:04 +0100

    movement added

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     [SerializeField]
-    public int jumpForce=1;
+    public int jumpForce=200;
+    [SerializeField]
+    private int speed = 10;
+    [SerializeField]
+    private bool canJump;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +19,51 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        movement();
+        jumIsNeeded();
+    }
+
+    private void jumIsNeeded()
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Espacio pulsado");
             this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
         }
     }
+
+    void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Time.deltaTime * Vector3.forward * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Time.deltaTime * Vector3.back * speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Time.deltaTime * Vector3.left * speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Time.deltaTime * Vector3.right * speed;
+        }
+    }
 }
+
+
+
+
+
