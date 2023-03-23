00a3dafc3bd3e8a5562d5b9947f8b7062a066dc9
Jonimarti <124839368+jonMartinezDeusto@users.noreply.github.com>
2023-03-23 09:07:28 +0100

    Raycast Added to shooter scene

--- Assets/shoot.cs
+++ Assets/shoot.cs
@@ -26,5 +26,29 @@ public class shoot : MonoBehaviour
         }
     }
 
-  
+    void FixedUpdate()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 10;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+         
+            Debug.Log("Did Hit" + hit.collider.gameObject.name);
+            
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
 }
+
