2d701e2be381741f159bbed2e62216cc4b6f983c
Jonimarti <124839368+jonMartinezDeusto@users.noreply.github.com>
2024-03-15 09:37:01 +0100

    new scene Spawner and jump scene new playground

---
+++ Assets/spawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{public GameObject[] spawneable;    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObjects();
+
+    }
+
+    private void SpawnObjects()
+    {
+        for (int i = 0; i < spawneable.Length; i++)
+        {
+            Instantiate(spawneable[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
