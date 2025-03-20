3414464f92cced153ca62d0d50d1bc04d700fd8d
jonMartinezDeusto <joninaki.martinez@deusto.es>
2025-03-20 09:04:34 +0100

    Scena 1 colliders

---
+++ Assets/CheckCollision.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name=="Capsule")
+        {
+            Debug.Log("Comienzo a chocar");
+            Destroy(this.gameObject);
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Capsule")
+        {
+            Debug.Log("Estoy chocando");
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Capsule")
+        {
+            Debug.Log("He dejado de chocar");
+        }
+       
+    }
+}
+
+
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    public void CheckMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, gameObject.transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, gameObject.transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+    }
+}
