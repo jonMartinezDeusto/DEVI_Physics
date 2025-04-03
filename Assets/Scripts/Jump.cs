bbddf50d4a19cd3ccf91777cc07de51ad97f6490
jonMartinezDeusto <joninaki.martinez@deusto.es>
2025-04-03 01:18:09 -0600

    New Jump Scene added

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    [SerializeField]
+    
     public int jumpForce=200;
+
     [SerializeField]
     private int speed = 10;
     [SerializeField]
